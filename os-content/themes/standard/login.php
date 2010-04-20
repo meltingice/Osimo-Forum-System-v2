71e879d83b13f22d648800d8f454f5910b675a0e
Ryan LeFevre <ryanlefevre@rlefevrewifi2.wifi.wpi.edu>
2010-04-20 15:54:40 -0400

    Updated the standard theme's login page finally to fit with the rest of the theme.

--- os-content/themes/standard/login.php
+++ os-content/themes/standard/login.php
@@ -1,14 +1,25 @@
 <? get('theme')->include_header(); ?>
 
 <div id="content">
-	<form id="login_form" action="<? get('theme')->login_action_url(); ?>" method="post">
-		<p class="login_label">username</p>
-		<input type="text" id="<? get('theme')->login_username_css_id(); ?>" name="<? get('theme')->login_username_name(); ?>" />
-		<p class="login_label">password</p>
-		<input type="password" id="<? get('theme')->login_password_css_id(); ?>" name="<? get('theme')->login_password_name(); ?>" />
-		<br />
-		<input type="submit" value="Login" />
-	</form>
+	<div id="login_wrap">
+		<div id="login_title">
+			<h1>Sign In</h1>
+		</div>
+		<div id="login_desc">
+			<h2>Enter your username and password</h2>
+		</div>
+		
+		<form id="login_form" action="<? get('theme')->login_action_url(); ?>" method="post">
+			<p class="login_label">username</p>
+			<input type="text" id="<? get('theme')->login_username_css_id(); ?>" name="<? get('theme')->login_username_name(); ?>" />
+			<p class="login_label">password</p>
+			<input type="password" id="<? get('theme')->login_password_css_id(); ?>" name="<? get('theme')->login_password_name(); ?>" />
+			
+			<div id="login_form_submit">
+				<input type="submit" value="Login" />
+			</div>
+		</form>
+	</div>
 </div>
 
 <? get('theme')->include_footer(); ?>
